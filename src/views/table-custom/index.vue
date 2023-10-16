fd344356571b1d9814a877c0ee2e403d68454fd3
Leslie Chan <leslie_chan@wochacha.com>
2023-10-16 16:19:51 +0800

    feat: 自定义table使用后端路由

--- src/views/table-custom/index.vue
+++ src/views/table-custom/index.vue
@@ -70,54 +70,61 @@ const params = ref({
 </script>
 
 <template>
-  <PureTableBar title="" :columns="columns">
-    <template v-slot="{ size, dynamicColumns }">
-      <el-table ref="tableRef" :data="cusData" :size="size" style="width: 100%">
-        <el-table-column
-          v-for="(column, index) in dynamicColumns"
-          :key="index"
-          :label="column.label"
-          :type="column.type"
-          :prop="column.prop"
-          :align="column.align ?? 'center'"
+  <div class="main">
+    <PureTableBar title="" :columns="columns">
+      <template v-slot="{ size, dynamicColumns }">
+        <el-table
+          ref="tableRef"
+          :data="cusData"
+          :size="size"
+          style="width: 100%"
         >
-          <template #header>
-            <el-col @click="headerFilter = !headerFilter">
-              {{ column.label }}
-            </el-col>
-            <el-col
-              v-if="column.meta?.filterType ?? false"
-              v-show="headerFilter"
-            >
-              <el-date-picker
-                v-if="column.meta.filterType == 'date'"
-                v-model="params[column.prop]"
-                type="date"
-                width="100%"
-                placeholder="Pick a day"
-              />
-              <el-select
-                v-else-if="column.meta.filterType == 'select'"
-                v-model="params[column.prop]"
-                clearable
-                multiple
-                collapse-tags
-                collapse-tags-tooltip
-                placeholder="请选择"
-                style="width: 240px"
+          <el-table-column
+            v-for="(column, index) in dynamicColumns"
+            :key="index"
+            :label="column.label"
+            :type="column.type"
+            :prop="column.prop"
+            :align="column.align ?? 'center'"
+          >
+            <template #header>
+              <el-col @click="headerFilter = !headerFilter">
+                {{ column.label }}
+              </el-col>
+              <el-col
+                v-if="column.meta?.filterType ?? false"
+                v-show="headerFilter"
               >
-                <el-option
-                  v-for="item in column.meta.selectOptions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
+                <el-date-picker
+                  v-if="column.meta.filterType == 'date'"
+                  v-model="params[column.prop]"
+                  type="date"
+                  width="100%"
+                  placeholder="Pick a day"
                 />
-              </el-select>
-              <el-input v-else v-model="params[column.prop]" />
-            </el-col>
-          </template>
-        </el-table-column>
-      </el-table>
-    </template>
-  </PureTableBar>
+                <el-select
+                  v-else-if="column.meta.filterType == 'select'"
+                  v-model="params[column.prop]"
+                  clearable
+                  multiple
+                  collapse-tags
+                  collapse-tags-tooltip
+                  placeholder="请选择"
+                  style="width: 240px"
+                >
+                  <el-option
+                    v-for="item in column.meta.selectOptions"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+                <el-input v-else v-model="params[column.prop]" />
+              </el-col>
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+    </PureTableBar>
+  </div>
 </template>
