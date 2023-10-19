d8d4c5bed394249a8fa6b8ae872cf43c55803c82
Leslie Chan <leslie_chan@wochacha.com>
2023-10-19 18:19:49 +0800

    fix: 删除无用代码

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,6 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
       proxy: {
         "/api": {
-          // 这里填写后端地址
           target: "http://127.0.0.1:8000",
           changeOrigin: true
           // rewrite: path => path.replace(/^\/proxy/, "")
